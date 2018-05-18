a7d61ffd438592eac2b48eee6069e1065f96ab82
MatrixStefan <stefan.ritchie@thisismatrix.com>
2018-05-18 15:41:22 +0100

    Users require approval after signing in - has to be done manually through console and no emails are sent out at the moment (I think)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_102333) do
+ActiveRecord::Schema.define(version: 2018_05_18_123241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2018_05_18_102333) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false, null: false
+    t.index ["approved"], name: "index_users_on_approved"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
