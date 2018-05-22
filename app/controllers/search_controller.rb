ec1ac2dda0a9bca00b572acf23e2bf95240bbf13
MatrixStefan <stefan.ritchie@thisismatrix.com>
2018-05-22 11:16:31 +0100

    Search now includes Release Note Items. Non-published results are now discounted if the user is not logged in. Change to the way the notices/alerts are displayed (not final)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,16 +7,25 @@ class SearchController < ApplicationController
         format.html {redirect_to :release_notes}
       end
     else
+      result_count = 0
       @search_results = ReleaseNote.search(params[:term], order: {release_date: :desc})
 
-      if @search_results.count == 0
+      if user_signed_in?
+        result_count = @search_results.count
+      else
+        @search_results.each do |sr|
+          result_count += 1 if sr.published
+        end
+      end
+
+      if result_count == 0
         @release_notes = ReleaseNote.all
         respond_to do |format|
           flash[:notice] = 'No search results found. Displaying everything...'
           format.html {redirect_to :release_notes}
         end
       else
-        flash[:notice] = 'We found ' + @search_results.count.to_s + ' results in ' + @search_results.took.to_s + ' milliseconds'
+        flash[:notice] = 'We found ' + result_count.to_s + ' results in ' + @search_results.took.to_s + ' milliseconds'
       end
     end
   end
--- app/models/release_note.rb
+++ app/models/release_note.rb
@@ -7,7 +7,20 @@ class ReleaseNote < ApplicationRecord
 
   validates :release_date, :title, :presence => true
 
-
   scope :published, -> {where(published: true)}
+
+  def search_data
+    {
+        title: title,
+        intro: intro,
+        outro: outro,
+        release_date: release_date,
+
+        #release_note_items (has_many)
+        release_note_item_types: release_note_items.map{|rni| (rni.change_type.name)},
+        release_note_item_titles: release_note_items.map(&:change_title),
+        release_note_item_details: release_note_items.map(&:change_details)
+    }
+  end
   
 end
