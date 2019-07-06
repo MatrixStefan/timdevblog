37945769a27da61ff8ad0226b8e22c1bb17773bd
alainritchie <alain.ritchie@thisismatrix.com>
2019-07-06 10:53:03 +0100

    Updated the specs to use dynamic value (static was deprecated)

--- spec/factories/release_note_items.rb
+++ spec/factories/release_note_items.rb
@@ -4,20 +4,20 @@ FactoryBot.define do
     change_title { Faker::Simpsons.quote}
     change_details { Faker::ChuckNorris.fact }
   end
-  
+
   # These specify our record as a change type
   trait :is_new_feature do
     change_type = FactoryBot.build(:change_type, :new_feature)
-    change_type_id change_type.id
+    change_type_id {change_type.id}
   end
-  
+
   trait :is_enhancement do
     change_type = FactoryBot.build(:change_type, :enhancement)
-    change_type_id change_type.id
+    change_type_id {change_type.id}
   end
-  
+
   trait :is_bug_fix do
     change_type = FactoryBot.build(:change_type, :bug_fix)
-    change_type_id change_type.id
+    change_type_id {change_type.id}
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,40 +4,40 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     job_title { Faker::Job.title }
-    password "password"
-    password_confirmation "password"
+    password {"password"}
+    password_confirmation {"password"}
   end
-  
+
   trait :is_approved do
-    approved true
+    approved {true}
   end
 
   trait :is_not_approved do
-    approved false
+    approved {false}
   end
-  
+
   trait :is_dev do
-    dev true
+    dev {true}
   end
 
   trait :is_not_dev do
-    dev false
+    dev {false}
   end
 
   trait :without_email do
-    email nil
+    email {nil}
   end
 
   trait :without_first_name do
-    first_name nil
+    first_name {nil}
   end
 
   trait :without_last_name do
-    last_name nil
+    last_name {nil}
   end
-  
+
   trait :without_job_title do
-    job_title nil
+    job_title {nil}
   end
 
   trait :with_nickname do
@@ -45,44 +45,44 @@ FactoryBot.define do
   end
 
   trait :pref_name_0 do
-    preferred_name 0
+    preferred_name {0}
   end
 
   trait :pref_name_1 do
-    preferred_name 1
+    preferred_name {1}
   end
 
   trait :pref_name_2 do
-    preferred_name 2
+    preferred_name {2}
   end
 
   trait :pref_name_3 do
-    preferred_name 3
+    preferred_name {3}
   end
 
   trait :pref_name_4 do
-    preferred_name 4
+    preferred_name {4}
   end
 
   trait :pref_name_5 do
-    preferred_name 5
+    preferred_name {5}
   end
 
   trait :pref_name_6 do
-    preferred_name 6
+    preferred_name {6}
   end
 
   trait :pref_name_7 do
-    preferred_name 7
+    preferred_name {7}
   end
 
   trait :pref_name_8 do
-    preferred_name 8
+    preferred_name {8}
   end
 
   trait :preferred_name_test do
-    first_name "Dwayne"
-    nickname "The Rock"
-    last_name "Johnson"
+    first_name {"Dwayne"}
+    nickname {"The Rock"}
+    last_name {"Johnson"}
   end
 end
